5a7826f06a3cb79e3fde002e8c354fd9e51d3b0a
3vladimir <ali.amirkaveh79@gmail.com>
2024-12-31 01:14:31 +0330

    project start

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Header() {
+    return(
+        <>
+        <div className="bg-red-800 gotichFont py-6 text-center">
+            <div className="text-6xl tracking-widest font-black">
+
+            M A F I A
+            </div>
+        </div>
+        </>
+    )
+}
